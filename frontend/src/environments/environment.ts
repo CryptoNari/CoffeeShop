c3985d401365baec963ee80c0f2c9b03f025a660
Andreas Vieten <cryptonari@outlook.com>
2021-10-11 09:17:33 +0200

    Frontend Setup

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'fseduc.eu.auth0.com', // the auth0 domain prefix
+    url: 'fseduc.eu', // the auth0 domain prefix
     audience: 'Coffee', // the audience set for the auth0 app
     clientId: 'b1mfMHZn3CU3w48AFq3I7XfFc2WUGC1W', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
